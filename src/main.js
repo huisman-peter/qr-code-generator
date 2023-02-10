efe74b530b985de644d29361f915caeb5dc59f82
Peter Huisman <huisman.peter@siteclx.com>
2023-02-10 09:49:25 +0100

    switch to yarn

--- src/main.js
+++ src/main.js
@@ -1,67 +1,51 @@
-import QRCodeStyling, {QRCode, Options} from "qr-code-styling";
-
+import QRCodeStyling from "qr-code-styling";
 import { save } from '@tauri-apps/api/dialog';
 import { writeBinaryFile } from '@tauri-apps/api/fs';
-
 import css from "./qr.css?inline";
-
-
 class PfzwQrCodeGenerator extends CustomElement {
-
-  private url: string = "https://pfzw.nl/";
-
-  private render = () => {
-    return <div>
-      <input type="url" value={this.url} oninput={this.onDataChange} />
-      <div id="QRContainer" afterCreate={this.onCreateQRElement}>
-      </div>
-      <button onclick={this.onDownloadButtonClick}>Download QR code</button>
-    </div>;
-  };
-
-  private onCreateQRElement = (n: HTMLElement) => {
-    this.qrCode = new QRCodeStyling(this.defaultStyling);
-    this.qrCode.append(n);
-  }
-
-  private onDataChange = (e: Event) => {
-    const htmlInput = e.target as HTMLInputElement;
-    this.url = htmlInput.value;
-    this.url = this.defaultStyling.data;
-    this.qrCode.update(this.defaultStyling);
-  }
-
-  private onDownloadButtonClick = async () => {
-    const defaultPath =  this.url.split("//")[1].replaceAll("/", "-").replaceAll("?", "-").replaceAll(".", "-").replaceAll('/', "-");
-    const path = await save({
-      filters: [{
-        name: "QR code",
-        // defaultPath: defaultPath,
-        extensions: ['svg']
-      }]
-    }).catch((e) => e);
-    if (path instanceof Error || !path){
-      console.log(path)
-      return;
-    }
-
-    var type = "application/svg+xml";
-    var blob = new Blob([this.qrCode._svg.outerHTML.replace("<svg ", "<svg xmlns=\"http://www.w3.org/2000/svg\" ")], { type });
-    const buffer = await blob.arrayBuffer();
-    const contents = new Uint8Array(buffer);
-    const saveResult = await writeBinaryFile({
-      contents ,
-      path
-    })
-    
-  }
-
-  private defaultStyling: Options;
-
-  private qrCode: QRCodeStyling;
-
-  private get logo() {
-    return "data:image/svg+xml;base64," + btoa(`<svg id="Laag_1" data-name="Laag 1" viewBox="0 0 156 54" xmlns="http://www.w3.org/2000/svg">
+    url = "https://pfzw.nl/";
+    render = () => {
+        return Maquette.h("div", null,
+            Maquette.h("input", { type: "url", value: this.url, oninput: this.onDataChange }),
+            Maquette.h("div", { id: "QRContainer", afterCreate: this.onCreateQRElement }),
+            Maquette.h("button", { onclick: this.onDownloadButtonClick }, "Download QR code"));
+    };
+    onCreateQRElement = (n) => {
+        this.qrCode = new QRCodeStyling(this.defaultStyling);
+        this.qrCode.append(n);
+    };
+    onDataChange = (e) => {
+        const htmlInput = e.target;
+        this.url = htmlInput.value;
+        this.url = this.defaultStyling.data;
+        this.qrCode.update(this.defaultStyling);
+    };
+    onDownloadButtonClick = async () => {
+        const defaultPath = this.url.split("//")[1].replaceAll("/", "-").replaceAll("?", "-").replaceAll(".", "-").replaceAll('/', "-");
+        const path = await save({
+            filters: [{
+                    name: "QR code",
+                    // defaultPath: defaultPath,
+                    extensions: ['svg']
+                }]
+        }).catch((e) => e);
+        if (path instanceof Error || !path) {
+            console.log(path);
+            return;
+        }
+        var type = "application/svg+xml";
+        var blob = new Blob([this.qrCode._svg.outerHTML.replace("<svg ", "<svg xmlns=\"http://www.w3.org/2000/svg\" ")], { type });
+        const buffer = await blob.arrayBuffer();
+        const contents = new Uint8Array(buffer);
+        const saveResult = await writeBinaryFile({
+            contents,
+            path
+        });
+    };
+    defaultStyling;
+    qrCode;
+    get logo() {
+        return "data:image/svg+xml;base64," + btoa(`<svg id="Laag_1" data-name="Laag 1" viewBox="0 0 156 54" xmlns="http://www.w3.org/2000/svg">
     <defs>
       <style>
         .cls-3{fill:#fff}
@@ -74,61 +58,52 @@ class PfzwQrCodeGenerator extends CustomElement {
     </g>
   </svg>
   `);
-  }
-
-  connectedCallback() {
-    this.defaultStyling = {
-      width: 1200,
-      height: 1200,
-      data: this.url,
-      // margin: 10,
-      qrOptions: {
-        mode: "Byte",
-        errorCorrectionLevel: "H",
-      },
-      imageOptions: {
-        hideBackgroundDots: false,
-        imageSize: 1,
-        margin: 0,
-      },
-      dotsOptions: {
-        type: "square",
-        color: "#393a3c",
-      },
-      backgroundOptions: {
-        color: "#ffffff",
-      },
-      // image: this.logo,
-      cornersSquareOptions: {
-        type: "extra-rounded",
-        color: "#404b96",
-      },
-      cornersDotOptions: {
-        type: "square",
-      },
-    };
-
-    const template = this.templateFromString(
-      `
+    }
+    connectedCallback() {
+        this.defaultStyling = {
+            width: 1200,
+            height: 1200,
+            data: this.url,
+            // margin: 10,
+            qrOptions: {
+                mode: "Byte",
+                errorCorrectionLevel: "H",
+            },
+            imageOptions: {
+                hideBackgroundDots: false,
+                imageSize: 1,
+                margin: 0,
+            },
+            dotsOptions: {
+                type: "square",
+                color: "#393a3c",
+            },
+            backgroundOptions: {
+                color: "#ffffff",
+            },
+            // image: this.logo,
+            cornersSquareOptions: {
+                type: "extra-rounded",
+                color: "#404b96",
+            },
+            cornersDotOptions: {
+                type: "square",
+            },
+        };
+        const template = this.templateFromString(`
       <style>${css}</style>
-      <div class="root"></div>`,
-      true,
-    );
-    this.shadowRoot.appendChild(template);
-    const rootElement = this.shadowRoot.querySelector(".root");
-    this.createProjector(rootElement, this.render);
-  }
-
-  static get observedAttributes(): string[] {
-    return ["data"];
-  }
+      <div class="root"></div>`, true);
+        this.shadowRoot.appendChild(template);
+        const rootElement = this.shadowRoot.querySelector(".root");
+        this.createProjector(rootElement, this.render);
+    }
+    static get observedAttributes() {
+        return ["data"];
+    }
 }
-
-customElements.define("pfzw-qr-code-generator", PfzwQrCodeGenerator)
-
+customElements.define("pfzw-qr-code-generator", PfzwQrCodeGenerator);
 // let greetInputEl: HTMLInputElement | null;
 // let greetMsgEl: HTMLElement | null;
-
 // async function greet() {
 //   if (greetMsgEl && greetInputEl) {
 //     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -137,7 +112,6 @@ customElements.define("pfzw-qr-code-generator", PfzwQrCodeGenerator)
 //     });
 //   }
 // }
-
 // window.addEventListener("DOMContentLoaded", () => {
 //   greetInputEl = document.querySelector("#greet-input");
 //   greetMsgEl = document.querySelector("#greet-msg");
